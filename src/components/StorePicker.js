01d9ac35f590a4e7fe5be280762748e49b19fbbb
Joan Currie <jecurrie8@gmail.com>
2021-03-04 16:52:56 -0500

    Initial commit

---
+++ src/components/StorePicker.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { getFunName } from "../helpers";
+class StorePicker extends React.Component {
+  constructor(props) {
+    super(props);
+    this.myInput = React.createRef();
+    this.getToStore = this.getToStore.bind(this);
+  }
+  getToStore(event) {
+    event.preventDefault();
+    console.log(this.myInput.current.value);
+  }
+  render() {
+    return (
+      <form className="store-selector" onSubmit={this.getToStore}>
+        <h2>Please enter a store</h2>
+        <input
+          type="text"
+          ref={this.myInput}
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
+        <button type="submit">Visit Store!</button>
+      </form>
+    );
+  }
+}
+export default StorePicker;
